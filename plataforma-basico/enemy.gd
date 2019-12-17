3cfa4cd1ccf94cba60abe3a402e700596a6806a0
João Lima <jpalvesl101@gmail.com>
2019-12-17 11:11:51 -0300

    Implementando um portal entre fases

---
+++ plataforma-basico/enemy.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+var flip = false
+var pos_inicial
+var pos_final
+const VELOCIDADE = 0.3
+
+func _ready():
+	$AnimatedSprite.play("default")
+	pos_inicial = $".".position.x
+	pos_final = pos_inicial + 100
+
+func _physics_process(delta):
+	if pos_inicial <= pos_final and !flip:
+		$".".position.x += VELOCIDADE
+		$AnimatedSprite.flip_h = true
+		if $".".position.x >= pos_final:
+			flip = true
+			
+	elif $".".position.x >= pos_inicial and flip:
+		$".".position.x -= VELOCIDADE
+		$AnimatedSprite.flip_h = false
+		if $".".position.x <= pos_inicial:
+			flip = false
